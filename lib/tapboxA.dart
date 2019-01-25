e47d52b20b628041e2b81a4aa7239a8fc37dab57
Ye Min Naing <thargyi74@gmail.com>
2019-01-25 14:52:22 +0630

    TapBox A Manage It own State

---
+++ lib/tapboxA.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class TapboxA extends StatefulWidget {
+  TapboxA({Key key}) : super(key: key);
+
+  @override
+  _TapboxAState createState() => _TapboxAState();
+}
+
+class _TapboxAState extends State<TapboxA> {
+  bool _active = false;
+
+  void _handleTap() {
+    setState(() {
+      _active = !_active;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: _handleTap,
+      child: Container(
+        width: 200.0,
+        height: 200.0,
+        decoration: BoxDecoration(
+            color: _active ? Colors.lightGreen[700] : Colors.grey[600]),
+        child: Center(
+          child: Text(
+            _active ? ' Active ' : ' InActive ',
+            style: TextStyle(fontSize: 32.0, color: Colors.white),
+          ),
+        ),
+      ),
+    );
+  }
+}
